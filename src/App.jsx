6b3a09a4e9a78a499338b5225be92baea9b66745
RukerDevps <86552741+Arun12kumar@users.noreply.github.com>
2025-05-29 21:04:51 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './sections/Hero'
+import ShowcaseSection from './sections/ShowcaseSection'
 
 const Welcome = () => {
   return (
-    <div>
-        <Hero/>
-    </div>
+    <>
+      <Hero/>
+      <ShowcaseSection/>
+    </>
   )
 }
 
